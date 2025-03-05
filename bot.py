7c6195ec6e1dab416b18f277375d162960184807
Belolipetsky <mefistofi098@gmail.com>
2025-03-05 19:56:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,19 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
-from handlers import start, course, card
+from handlers import start, course, card, admin  # Импортируем все обработчики
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
+    # Подключаем обработчики
     dp.include_router(start.router)
     dp.include_router(course.router)
     dp.include_router(card.router)
+    dp.include_router(admin.router)
 
+    print("Бот запущен")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
