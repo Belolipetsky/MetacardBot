1bc5d6744c8f40ca870b0da47f8a578871b100e8
Belolipetsky <mefistofi098@gmail.com>
2025-03-08 01:47:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,14 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-import os
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 API_TOKEN = os.getenv("API_TOKEN")
 if not API_TOKEN:
-    raise Exception("API_TOKEN is not set in .env file")
+    raise Exception("API_TOKEN не задан в .env файле")
 
 logging.basicConfig(level=logging.INFO)
 
@@ -17,18 +17,16 @@ bot = Bot(token=API_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 
-# Импорт роутеров (обработчиков)
-from handlers import auth, card, payments, session, admin, start
+# Импортируем обработчики
+from handlers import auth, card, payments, session
 
 dp.include_router(auth.router)
-dp.include_router(start.router)  # Дополнительный обработчик для команды /home, если нужен
 dp.include_router(card.router)
 dp.include_router(payments.router)
 dp.include_router(session.router)
-dp.include_router(admin.router)
 
 async def main():
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     asyncio.run(main())
