a1e3c82809f385dc0a7177efd0c5c8457dd54baa
Belolipetsky <mefistofi098@gmail.com>
2025-03-08 01:29:03 +0300

    Update spreadsheet.py

--- utils/spreadsheet.py
+++ utils/spreadsheet.py
@@ -1,9 +1,9 @@
 # utils/spreadsheet.py
 
 def log_registration(user_data: dict):
-    # Здесь должна быть интеграция с Google Sheets
+    # Заглушка для логирования регистрации в Google Таблицы
     print("LOG (registration):", user_data)
 
 def log_payment(user_id: int, attempts_purchased: int, amount: int):
-    # Заглушка для логирования оплаты в Google Таблицы
+    # Заглушка для логирования платежей в Google Таблицы
     print(f"LOG (payment): user_id={user_id}, attempts={attempts_purchased}, amount={amount}")
